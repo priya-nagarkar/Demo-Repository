6beeadc227899158eaa6fb5aadad945ba0f2c4f1
Priya <priyannagarkar.com>
2020-09-27 17:11:16 +0000

    Second Commit

--- demo.java
+++ demo.java
@@ -1,7 +1,9 @@
 class Demo1 {
 
-public static void main (String arg[]){
-	system.out.println("This is Java class Demo1");
+public static void main(String arg[]){
+	System.out.println("This is Java class Demo1");
+System.out.println("This is second line output from Java class Demo1");
+}
 }
 
 
