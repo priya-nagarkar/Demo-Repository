67736181b85c10500a94a2ffef567d57976b0558
Priya <priyannagarkar.com>
2020-09-29 17:42:16 +0000

    ---additig demo1 to local repo now#

--- demo1.java
+++ demo1.java
@@ -3,6 +3,7 @@ class Demo1 {
 public static void main(String arg[]){
 	System.out.println("This is GIT learning from Demo1");
 System.out.println("This is GIT learning line output from Java class Demo1");
+System.out.println("this is after first commit");
 }
 }
 
