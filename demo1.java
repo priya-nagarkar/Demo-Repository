9c1f35e73568a25a975030a96827c74084c32617
Priya <priyannagarkar.com>
2020-09-29 16:57:24 +0000

    demo1.java added initially

---
+++ demo1.java
@@ -0,0 +1,13 @@
+class Demo1 {
+
+public static void main(String arg[]){
+	System.out.println("This is GIT learning from Demo1");
+System.out.println("This is GIT learning line output from Java class Demo1");
+}
+}
+
+
+
+
+
+	
